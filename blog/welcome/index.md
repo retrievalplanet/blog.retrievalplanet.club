2175b0a371de2b160e2dc7fd6f2e06c4a8718bfb
taoshengshi <taoshengshi01@gmail.com>
2023-05-31 18:23:35 +0800

    update draft true

--- blog/welcome/index.md
+++ blog/welcome/index.md
@@ -2,7 +2,7 @@
 slug: welcome
 title: Welcome
 authors: [taoshengshi]
-tags: [retrieval planet, hello, welcome, draft]
+tags: [retrieval planet, hello, welcome,draft]
 ---
 
 ** 欢迎来到检索星球！**
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          draft: true,
           blogSidebarCount: "ALL",
           showReadingTime: true,
           // Please change this to your repo.
